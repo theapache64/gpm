2ed1f7f6289d1ef2e17b67570aa9cef40eb80d18
theapache64 <theapache64@gmail.com>
2020-05-08 07:27:42 +0530

    🌟 add docs

--- src/main/kotlin/com/theapache64/gpm/commands/subcommands/docs/DocsComponent.kt
+++ src/main/kotlin/com/theapache64/gpm/commands/subcommands/docs/DocsComponent.kt
@@ -1,15 +1,20 @@
 package com.theapache64.gpm.commands.subcommands.docs
 
 import com.theapache64.gpm.core.TransactionManager
+import com.theapache64.gpm.di.GpmJsonFile
 import com.theapache64.gpm.di.modules.NetworkModule
 import com.theapache64.gpm.di.modules.TransactionModule
 import dagger.Component
+import java.io.File
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [TransactionModule::class, NetworkModule::class])
 interface DocsComponent {
+
     fun transactionManager(): TransactionManager
     fun inject(docs: Docs)
 
+    @GpmJsonFile
+    fun gpmJsonFile(): File
 }
--- src/test/kotlin/com/theapache64/gpm/commands/subcommands/docs/DocsTest.kt
+++ src/test/kotlin/com/theapache64/gpm/commands/subcommands/docs/DocsTest.kt
@@ -7,13 +7,16 @@ import com.theapache64.gpm.di.modules.TransactionModule
 import com.winterbe.expekt.should
 import it.cosenonjaviste.daggermock.DaggerMock
 import it.cosenonjaviste.daggermock.InjectFromComponent
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import picocli.CommandLine
+import java.io.File
 
 class DocsTest {
 
+    private lateinit var gpmJsonFile: File
     private lateinit var docsCmd: CommandLine
     private val docs = Docs(true)
 
@@ -24,12 +27,18 @@ class DocsTest {
     val daggerRule = DaggerMock.rule<DocsComponent>(TransactionModule(true)) {
         set {
             tm = it.transactionManager()
+            gpmJsonFile = it.gpmJsonFile()
         }
     }
 
     @Before
     fun setUp() {
         this.docsCmd = CommandLine(docs)
+    }
+
+    @After
+    fun tearDown() {
+        gpmJsonFile.delete()
     }
 
     @Test
@@ -58,11 +67,47 @@ class DocsTest {
 
     @Test
     fun `Opening docs for not installed dependency`() {
-
+        val exitCode = docsCmd.execute("not-installed-dep")
+        exitCode.should.equal(DocsViewModel.RESULT_NOT_FOUND)
     }
 
     @Test
     fun `Opening docs for dependency installed with similar name`() {
+        tm.add(
+            "okhttp",
+            GradleDep.Type.IMP,
+            GpmDep(
+                "okhttp",
+                GradleDep.Type.IMP.key,
+                "https://square.github.io/okhttp/",
+                "jcenter",
+                "https://github.com/square/okhttp/",
+                "com.square.okhttp3",
+                "Material Colors",
+                "A networking library",
+                "1.0.0"
+            )
+        )
 
+        tm.add(
+            "okhttp",
+            GradleDep.Type.IMP,
+            GpmDep(
+                "some-other-lib",
+                GradleDep.Type.IMP.key,
+                "https://square.github.io/okhttp/",
+                "jcenter",
+                "https://github.com/square/okhttp/",
+                "some-other-lib",
+                "some-other-lib",
+                "Some lib",
+                "1.0.0"
+            )
+        )
+
+        val exitCode = docsCmd.execute("okhttp")
+        exitCode.should.equal(DocsViewModel.RESULT_DOC_FOUND)
     }
+
+
 }
